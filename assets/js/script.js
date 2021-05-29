42c042472371400a7b380b127fae7c8a37674479
Subodh Ghulaxe <subodhghulaxe@gmail.com>
2021-05-30 00:20:42 +0530

    Added theme-color meta property

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,14 @@
+var mode = "light";
 document.getElementById('toggle').onclick = function() {
-  document.getElementsByTagName('body')[0].classList.toggle('dark');
-  document.getElementsByTagName('body')[0].classList.toggle('light');
+  if (mode == "light") {
+    document.querySelector('body').classList.remove('light');
+    document.querySelector('body').classList.add('dark');
+    document.querySelector('meta[name="theme-color"]').content = "#0E1117";
+    mode = "dark";
+  } else {
+    document.querySelector('body').classList.remove('dark');
+    document.querySelector('body').classList.add('light');
+    document.querySelector('meta[name="theme-color"]').content = "#FFFFFF";
+    mode = "light";
+  }
 }
