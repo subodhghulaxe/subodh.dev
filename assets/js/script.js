b167a980b4ca604e4213f20da3cf55686c2fe087
Subodh Ghulaxe <subodhghulaxe@gmail.com>
2021-06-02 12:18:03 +0530

    SEO improvements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,20 @@
 var mode = "light";
+document.addEventListener("DOMContentLoaded", function(event) {
+  var darkTheme = window.matchMedia("(prefers-color-scheme: dark)");
+  if (darkTheme.matches) mode = "dark"; else mode = "light";
+  if (mode == "light") {
+    document.querySelector('body').classList.add('light');
+    document.querySelector('body').classList.remove('dark');
+    document.querySelector('meta[name="theme-color"]').content = "#FFFFFF";
+    document.getElementById('toggle').checked = true;
+  } else {
+    document.querySelector('body').classList.add('dark');
+    document.querySelector('body').classList.remove('light');
+    document.querySelector('meta[name="theme-color"]').content = "#0E1117";
+    document.getElementById('toggle').checked = false;
+  }
+});
+
 document.getElementById('toggle').onclick = function() {
   if (mode == "light") {
     document.querySelector('body').classList.remove('light');
